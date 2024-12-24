9e96d0a3430720d02d5f3bbe3204c08f7859b4c3
cyber-ribouldingue <ribouldingue@tutanota.com>
2024-12-24 07:08:21 +0100

    Update tests_exo1_c.sh

--- tests/c/tests_exo1_c.sh
+++ tests/c/tests_exo1_c.sh
@@ -1,1 +1,16 @@
+#!/usr/bin/env bash
+
+# tests/c/tests_exo1_c.sh
+
+# On s'attend à la sortie "La somme est 5"
+
+gcc -o exo1 snippets/c/exo1.c
+output=$(./exo1)
+if [ "$output" = "La somme est 5" ]; then
+  echo "Test exo1 C: OK"
+else
+  echo "Test exo1 C: ECHEC - sortie obtenue: '$output'"
+fi
+
+rm -f exo1
 
