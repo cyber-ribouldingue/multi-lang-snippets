d3b845c62b5373502fb96e7c8201d2623a63db1a
cyber-ribouldingue <ribouldingue@tutanota.com>
2024-12-24 06:23:44 +0100

    Create snippet1.cpp

---
+++ snippets/cpp/snippet1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Exercice: afficher "Hello C++"
+    // Erreur: on oublie le "<<" entre cout et la chaîne
+    cout "Hello C++\n";
+    return 0;
+}
