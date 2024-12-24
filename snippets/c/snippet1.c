45fccfaa9e4443854db4dc8128542db324caf55b
cyber-ribouldingue <ribouldingue@tutanota.com>
2024-12-24 06:22:10 +0100

    Create snippet1.c

---
+++ snippets/c/snippet1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // Exercice: afficher "Hello C" à l'écran.
+
+    // ERREUR: Il manque un point-virgule, par exemple
+    printf("Hello C\n")
+    
+    return 0;
+}
