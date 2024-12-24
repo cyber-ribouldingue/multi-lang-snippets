c7416d240108085af85e730389be978fba564d99
cyber-ribouldingue <ribouldingue@tutanota.com>
2024-12-24 06:55:16 +0100

    Delete snippets/cpp directory

--- snippets/cpp/snippet1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    // Exercice: afficher "Hello C++"
-    // Erreur: on oublie le "<<" entre cout et la chaîne
-    cout "Hello C++\n";
-    return 0;
-}
